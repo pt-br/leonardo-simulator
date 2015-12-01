508e2ea8c9d8230b9190b497336a703080e99001
pt-br <lucas.paixao@objectedge.com>
2015-12-01 00:00:09 -0200

    First adjustments

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  /// GLOBAL VARS BEGIN ///
+
+  /// GLOBAL VARS END ///
+
+  $(document)
+    .on("ready", function() {
+      whatEver();
+    });
+
+  function whatEver() {
+    console.log("Leonardo Simulator 2000"); 
+  }
+
+});
