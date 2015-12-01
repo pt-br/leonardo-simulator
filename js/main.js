fce4e4f6c8cee949033832d999f03074974976a5
pt-br <lucas.paixao@objectedge.com>
2015-12-01 18:42:57 -0200

    Adding text

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(function() {
 
   /// GLOBAL VARS BEGIN /// 
-  var simImages = ["barbie.png", "batman.png", "cowboy.png", "news.png", "pizza.png", "tekpix.png"];
-  var positionText = [[395, 55], [130, 338], [125, 62], [135, 365], [370, 355], [368, 70], ];
   var imagePath = "./assets/optimized/";
+  var simImages = ["barbie.png", "batman.png", "cowboy.png", "news.png", "pizza.png", "tekpix.png"];
+  var positionText = [[395, 55], [130, 338], [125, 62], [135, 365], [370, 355], [368, 70]];
+  var jokes = ["Faz cosplay de android que eu vou fazer cosplay de ios",
+               "Vou jogar um dado assistindo porn",
+               "Se eu tenho um dinossaurinho de plástico, na verdade é um dinossauro morto",
+               "Tá matando a mulher... matando de amor"
+              ];
   var imageInUse;
+  var jokeInUse;
   /// GLOBAL VARS END ///
 
   $(document)
@@ -31,7 +37,12 @@ $(function() {
     if(imageInUse != newImage) {
       currentSim.css("background", "url(" + imagePath + newImage + ") no-repeat");
       imageInUse = newImage;
+
       simText.css({"top" : newTextPosTop, "left" : newTextPosLeft});
+
+      var newJoke = refreshJoke();
+
+      simText.text(newJoke);
     } else {
       refreshImage();
     }
@@ -44,5 +55,20 @@ $(function() {
     tvButton.attr("rotation", newDeg);
     tvButton.css("transform", "rotate(" + newDeg + "deg)");
   }
+
+  function refreshJoke() {
+    var maxJokeRange = jokes.length;
+    var newJokeId = Math.floor(Math.random() * maxJokeRange);
+    var newJoke = jokes[newJokeId];
+
+    if(jokeInUse != newJoke) {
+      jokeInUse = newJoke;
+      return newJoke;
+    } else {
+      refreshJoke();
+    }
+  }
+
+
 
 });
